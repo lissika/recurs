312d4f89b194949469faf164482fda1ec3607f6a
marina <lissicats@list.ru>
2022-07-13 19:22:08 +0300

    ex01

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу, которая переворачивает массив.Первый элемент с последним и т.д.
+Console.WriteLine("Задайте длину массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [n];
+for (int i = 0; i < array.Length; i++)
+{
+     array[i] = new Random().Next(100,999);
+}
+Console.WriteLine("["+string.Join(",",array)+ "]");
+for (int i = 0 ; i < array.Length/2; i++)
+{
+   (array[i], array[array.Length - (i+1)]) = (array[array.Length - (i+1)], array[i]); 
+}
+Console.WriteLine("Массив перевернут: ");
+Console.WriteLine("["+string.Join(",",array)+ "]");
